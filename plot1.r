83f7369c909f4d818ab6e9b06377eebe64d892be
FreddieBeagle <seleblanc@gmail.com>
2017-02-19 18:23:18 -0500

    Add files via upload

---
+++ plot1.r
@@ -0,0 +1,11 @@
+dataFile <- "./household_power_consumption.txt"
+## The file is my working directory
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## We have now subset the data by the two dates required
+## Let's plot it
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
